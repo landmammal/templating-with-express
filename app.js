74e0017b86afcf30f17c0936ad8767018e2c26da
Roger <idevelopedreams@gmail.com>
2019-06-26 10:13:24 -0400

    setting ejs as my view engine

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
+// adding middleware
+app.set("view engine", "ejs"); // setting up templating engine
+
+app.get('/home', function (req, res) {
+  res.render('home');
 });
  
 app.listen(3000, function(err){
